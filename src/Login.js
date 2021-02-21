b93c89b0445cf7e37840382cdc29958d0b191ed3
vasilije7 <vasilijev7@gamil.com>
2021-02-21 22:18:50 +0100

    napravljena html'+ js login forma

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
-
-class Login extends Component {
+import React from "react";
+import { Component } from "react";
+class Login extends React.Component {
+  state = {
+    username: "",
+    pwd: "",
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
-    return <h1>Login 12</h1>;
+    return (
+      <div className="login">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="username"
+            name="username"
+            placeholder="username..."
+            requried
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="pwd"
+            placeholder="password"
+            requried
+            onChange={this.handleChange}
+          />
+          <button onSubmit={this.handleSubmit}>log in</button>
+        </form>
+      </div>
+    );
   }
 }
-
 export default Login;
