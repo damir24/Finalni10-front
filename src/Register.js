1ccf8a68b9c097715d06647c076b210d4f991f70
Damir Pilica <pilica0000@gmail.com>
2021-02-23 21:09:59 +0100

    Uradjena register stranica bez CSS-a

--- src/Register.js
+++ src/Register.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
-
-class Register extends Component {
-    render() {
-        return <h1>Register</h1>
-    }
+import React from "react";
+class Register extends React.Component {
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  state = {
+    username: "",
+    pwd: "",
+    email: "",
+  };
+  render() {
+    return (
+      <div className="register">
+        <form onSubmit={this.handleSubmit}>
+        <input
+            name="username"
+            type="username"
+            placeholder="username"
+            onChange={this.handleChange}
+          />
+          <br></br>
+         <input
+            type="password"
+            name="pwd"
+            placeholder="password"
+            onChange={this.handleChange}
+          />
+          <br></br>
+           <input
+            name="email"
+            type="email"
+            placeholder="type your email"
+            onChange={this.handleChange}
+          />
+          <br></br>
+          <button
+          onSubmit={this.handleSubmit}>
+          Click here for register
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default Register;
