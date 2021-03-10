c0eb953d70571d9742e2bc9f7bcc2e9c7df63938
Damir Pilica <pilica0000@gmail.com>
2021-03-10 21:17:18 +0100

    Register odradjen jos api

--- src/Register.js
+++ src/Register.js
@@ -1,140 +1,66 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function SignUp() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+import React, { useState } from 'react'  
+import axios from 'axios';  
+function Regster(props) {  
+  const [data, setdata] = useState({ Email: '', Password: '',Name: '', })  
+  const apiUrl = "http://localhost:3000/";  
+  const Registration = (e) => {  
+    e.preventDefault();  
+    debugger;  
+    const data1 = { Email: data.Email, Password: data.Password, Name: data.EmployeeName, };  
+    axios.post(apiUrl, data1)  
+      .then((result) => {  
+        debugger;  
+        console.log(result.data);  
+        if (result.data.Status == 'Invalid')  
+          alert('Invalid User');  
+        else  
+          props.history.push('/Dashboard')  
+      })  
+  }  
+  const onChange = (e) => {  
+    e.persist();  
+    debugger;  
+    setdata({ ...data, [e.target.name]: e.target.value });  
+  }  
+  return (  
+    <div class="container">  
+      
+  
+      <div class="card o-hidden border-0 shadow-lg my-5" style={{ "marginTop": "5rem!important;" }}>  
+        <div class="card-body p-0">  
+          <div class="row">  
+            <div class="col-lg-12">  
+              <div class="p-5">  
+                <div class="text-center">  
+                  <h1 class="h4 text-gray-900 mb-4">Novi korisnik</h1>  
+                </div>  
+                <form onSubmit={Registration} class="user">  
+                  <div class="form-group row">  
+                    <div class="col-sm-6 mb-3 mb-sm-0">  
+                      <input type="text" name="Email" onChange={onChange} value={data.Email} class="form-control" id="exampleFirstName" placeholder="Email" />  
+                    </div>  
+                    <div class="col-sm-6">  
+                      <input type="Password" name="Password" onChange={onChange} value={data.Password} class="form-control" id="exampleLastName" placeholder="Password" />  
+                    </div>  
+                  </div>  
+                  <div class="form-group">  
+                    <input type="text" name="Name" onChange={onChange} value={data.Name} class="form-control" id="exampleInputEmail" placeholder="Name" />  
+                  </div>  
+                  
+                    
+                  <button type="submit" class="btn btn-primary  btn-block">  
+                    Registruj se
+                </button>  
+                  <hr />  
+                </form>  
+                <hr />  
+              </div>  
+            </div>  
+          </div>  
+        </div>  
+      </div>  
+    </div>  
+  )  
+}  
+  
+export default Regster  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from "./components/navigation.js";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 ReactDOM.render(
     <React.StrictMode>
